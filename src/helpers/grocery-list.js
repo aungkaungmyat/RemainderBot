80c95dc05b73d9ac399891a82804330e804b4f41
mthormati <mthormati@gmail.com>
2019-08-27 12:42:59 -0400

    Create update function

--- src/helpers/grocery-list.js
+++ src/helpers/grocery-list.js
@@ -3,6 +3,7 @@ const verificationError = require('./verificationException');
 const grocerySchema = require('../models/grocery-schema');
 
 const db = mongoose.connection;
+const Grocery = mongoose.model('Grocery', grocerySchema);
 
 module.exports = {
   handleMessage(userId, message) {
@@ -13,8 +14,11 @@ module.exports = {
       case 'list':
         return getList();
       case 'rm':
+        return removeItem(command);
       case 'clear':
+        return removeAllItems();
       case 'update':
+        return updateItem(command);
       case 'default':
         return new verificationError('No match', 'use help --grocery to get grocery list commands.');
     }
@@ -23,28 +27,30 @@ module.exports = {
 
 function addToList(userId, command) {
   // Check for any errors in the input
-  const verificationErr = verifyItem(command);
-  const response = 'hi';
+  const verificationErr = verifyAdd(command);
   if (verificationErr == null) {
     // Create new item with information from function parameters
-    const Grocery = mongoose.model('Grocery', grocerySchema);
     const grocery = new Grocery({
       userId: userId,
       count: command[1],
       name: command[2]
     });
     // Save new grocery item to database
-    return grocery.save();
+    return grocery.save()
+      .then(data => {
+        return command[2] + ' added';
+      });
   } else {
     return Promise.reject(verificationErr.message + '\n' + verificationErr.cause);
   }
 }
 
 function getList() {
-  const Grocery = mongoose.model('Grocery', grocerySchema);
+  // Get all documents in grocery collection
   return Grocery.find({}, null)
     .then(data => {
-      response = ''
+      response = '';
+      // Map over documents and create list
       data.map(item => {
         response += item.name + ': ' + item.count + '\n';
       });
@@ -52,15 +58,106 @@ function getList() {
     });
 }
 
-function removeItem() {
-
+function removeItem(command) {
+  // Check for any errors in input
+  const verificationErr = verifyRemove(command);
+  if (verificationErr == null) {
+    // Remove item
+    return Grocery.deleteOne({name: command[1]})
+      .then(data => {
+        if (data.deletedCount > 0) {
+          return 'item deleted';
+        } else {
+          return 'item not found';
+        }
+      });
+  } else {
+    return Promise.reject(verificationErr.message + '\n' + verificationErr.cause);
+  }
 }
 
-// Verify command for add and update
-// Returns a verification error or null
-function verifyItem(command) {
+function removeAllItems() {
+  return Grocery.deleteMany({})
+    .then(data => {
+      return 'the grocery list has been cleared';
+    });
+}
+
+function updateItem(command) {
+  const newGrocery = createUpdatedItem(command);
+  if (newGrocery instanceof Grocery) {
+    if (newGrocery.count == -1) {
+      return Grocery.findOneAndUpdate({name: command[1]}, 
+        {name: newGrocery.name},
+        {useFindAndModify: false})
+        .then(data => {
+          if (data == null) {
+            return 'item not found';
+          } else {
+            return 'item has been updated'
+          }
+        });
+    } else if (newGrocery.name == '') {
+      return Grocery.findOneAndUpdate({name: command[1]}, 
+        {count: newGrocery.count},
+        {useFindAndModify: false})
+        .then(data => {
+          if (data == null) {
+            return 'item not found';
+          } else {
+            return 'item has been updated'
+          }
+        });
+    } else {
+      return Grocery.findOneAndUpdate({name: command[1]},
+         {name: newGrocery.name, count: newGrocery.count},
+         {useFindAndModify: false})
+        .then(data => {
+          if (data == null) {
+            return 'item not found';
+          } else {
+            return 'item has been updated'
+          }
+        });
+    }
+  } else {
+    return Promise.reject(newGrocery.message + '\n' + newGrocery.cause);
+  }
+}
+
+function createUpdatedItem(command) {
+  if (command.length == 3) {
+    if (isNaN(command[2])) {
+      return new Grocery({
+        userId: '',
+        count: -1,
+        name: command[2]
+      });
+    } else {
+      return new Grocery({
+        userId: '',
+        count: Number.parseInt(command[2]),
+        name: ''
+      });
+    }
+  } else if (command.length == 4) {
+    if (!isNaN(command[2]) && Number.parseInt(command[2]) > 0) {
+      return new Grocery({
+        userId: '',
+        count: Number.parseInt(command[2]),
+        name: command[3]
+      })
+    } else {
+      return new verificationError('Incorrent count', 'item count must an integer > 0');
+    }
+  } else {
+    return new verificationError('Invalid number of arguments', 'usage: update <item name> <new count> <new name>');
+  }
+}
+
+function verifyAdd(command) {
   if (command.length != 3) {
-    return new verificationError('Not enough arguments', 'usage: add <item count> <item>');
+    return new verificationError('Invalid number of arguments', 'usage: add <item count> <item>');
   } else if (isNaN(command[1])) {
     return new verificationError('Incorrect item count', 'item count must be an integer');
   } else if (Number.parseInt(command[1]) <= 0) {
@@ -70,6 +167,10 @@ function verifyItem(command) {
   }
 }
 
-function verifyRemove() {
-
+function verifyRemove(command) {
+  if (command.length != 2) {
+    return new verificationError('Invalid number of arguments', 'usage: rm <item>');
+  } else {
+    return null;
+  }
 }
--- src/tests/grocery-list-spec.js
+++ src/tests/grocery-list-spec.js
@@ -1,16 +1,41 @@
 const groceryList = require('../helpers/grocery-list');
 
-
-// groceryList.handleMessage('6789', 'add -1 sock')
-//   .then((item) => {
-//     console.log('item added');
+// groceryList.handleMessage('6789', 'add 1 sock')
+//   .then(response => {
+//     console.log(response);
 //   })
 //   .catch((err) => {
 //     console.log(err);
 //   });
 
 // groceryList.handleMessage('6789', 'list')
-//   .then(items => {
-//     console.log(items);
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// groceryList.handleMessage('678 9', 'rm sock')
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
 //   })
 
+// groceryList.handleMessage('6789', 'clear')
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+groceryList.handleMessage('6789', 'update')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
